63a44bcf7ac8fee5d6d01e31b29bc6c690074f70
SN\blaskod <blaskoi@gmail.com>
2019-07-23 14:52:35 +0200

    List and hover

--- src/components/mainpanel.tsx
+++ src/components/mainpanel.tsx
@@ -1,20 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemAvatar from '@material-ui/core/ListItemAvatar'
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
-import ListItemText from '@material-ui/core/ListItemText'
-import Avatar from '@material-ui/core/Avatar'
 import { ContentList, ContentListProps } from '@sensenet/list-controls-react'
-import IconButton from '@material-ui/core/IconButton'
 import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
-import FolderIcon from '@material-ui/icons/Folder'
-import DeleteIcon from '@material-ui/icons/Delete'
-import { GenericContent, SchemaStore } from '@sensenet/default-content-types'
+import CloudDownload from '@material-ui/icons/CloudDownload'
+import OpenInBrowser from '@material-ui/icons/OpenInBrowser'
+import Edit from '@material-ui/icons/Edit'
+import { File, SchemaStore } from '@sensenet/default-content-types'
+import { ConstantContent, ODataCollectionResponse } from '@sensenet/client-core'
+import { TableCell } from '@material-ui/core'
+import { useRepository } from '../hooks/use-repository'
+import { icons } from '../assets/icons'
 
-export interface ContentListDocState extends ContentListProps<GenericContent> {
+export interface ContentListDocState extends ContentListProps<File> {
   isEditing: boolean
 }
 
@@ -26,83 +23,84 @@ const useStyles = makeStyles((theme: Theme) =>
     title: {
       margin: theme.spacing(4, 0, 2),
     },
+    actionicon: {
+      marginRight: '10px',
+    },
   }),
 )
 
-function generate(element: React.ReactElement) {
-  return [0, 1, 2].map(value =>
-    React.cloneElement(element, {
-      key: value,
-    }),
-  )
-}
-
 const MainPanel: React.FunctionComponent = () => {
-  const state: ContentListDocState = {
-    items: [
-      { Id: 1, Path: '/Root/Examples/Foo', Type: 'Folder', Name: 'Foo', DisplayName: 'FoOoOo', Icon: 'file' },
-      { Id: 2, Path: '/Root/Examples/Bar', Type: 'Folder', Name: 'Bar', DisplayName: 'Bár', Icon: 'Settings' },
-      { Id: 3, Path: '/Root/Examples/Baz', Type: 'Folder', Name: 'Baz', DisplayName: 'Z Baz', Icon: 'File' },
-    ],
-    schema: SchemaStore.filter(s => s.ContentTypeName === 'GenericContent')[0],
-    selected: [],
-    icons: { file: 'insert_drive_file', settings: 'settings' },
-    fieldsToDisplay: ['DisplayName', 'Name', 'Type', 'Id'],
-    orderBy: 'Id',
-    orderDirection: 'asc',
-    isEditing: false,
-  }
   const classes = useStyles()
+  const repo: any = useRepository()
+  const [data, setData] = useState<File[]>([])
+
+  useEffect(() => {
+    async function loadDocuments(): Promise<void> {
+      const result: ODataCollectionResponse<File> = await repo.loadCollection({
+        path: `${ConstantContent.PORTAL_ROOT.Path}/Content/IT/Document_Library`,
+        oDataOptions: {
+          filter: "ContentType eq 'File' or ContentType eq 'Folder'",
+          select: [
+            'DisplayName',
+            'Description',
+            'CreationDate',
+            'CreatedBy',
+            'ModificationDate',
+            'Icon',
+            'Type',
+            'Id',
+            'Path',
+            'Name',
+            'Size',
+            'Actions',
+          ] as any,
+          orderby: [['ModificationDate', 'desc']],
+          expand: ['CreatedBy', 'Actions'] as string[],
+        },
+      })
+
+      // Fix file unit symbol
+      result.d.results.map(content => {
+        if (content.Size !== null && content.Size !== undefined) {
+          const fixedsize = content.Size / 1024 / 1024 // convert to MB
+          content.Size = parseFloat(fixedsize.toFixed(2))
+        }
+      })
+      setData(result.d.results)
+    }
+
+    // Load documents from Repository
+    loadDocuments()
+  }, [repo])
+
   return (
     <div>
       <Grid container>
         <Grid item xs={12}>
-          <Typography variant="h6" className={classes.title}>
-            Documents
-          </Typography>
-          <div className={classes.demo}>
-            <List>
-              {generate(
-                <ListItem>
-                  <ListItemAvatar>
-                    <Avatar>
-                      <FolderIcon />
-                    </Avatar>
-                  </ListItemAvatar>
-                  <ListItemText primary="Single-line item" secondary={'Secondary text'} />
-                  <ListItemSecondaryAction>
-                    <IconButton edge="end" aria-label="Delete">
-                      <DeleteIcon />
-                    </IconButton>
-                    <IconButton edge="end" aria-label="Delete">
-                      <DeleteIcon />
-                    </IconButton>
-                  </ListItemSecondaryAction>
-                </ListItem>,
-              )}
-            </List>
-          </div>
-        </Grid>
-        <Grid item xs={12}>
-          <ContentList
-            displayRowCheckbox={true}
-            schema={state.schema}
-            selected={state.selected}
-            active={state.active}
-            items={state.items}
-            fieldsToDisplay={['DisplayName']}
-            orderBy={'DisplayName'}
-            orderDirection={'desc'}
-            onRequestSelectionChange={state.onRequestSelectionChange}
-            onRequestActiveItemChange={state.onRequestActiveItemChange}
-            onRequestActionsMenu={state.onRequestActionsMenu}
-            onItemContextMenu={state.onItemContextMenu}
-            onRequestOrderChange={state.onRequestOrderChange}
-            onItemClick={state.onItemClick}
-            onItemDoubleClick={state.onItemDoubleClick}
+          <ContentList<File>
+            displayRowCheckbox={false}
+            schema={SchemaStore.find(s => s.ContentTypeName === 'File') as any}
+            selected={[]}
+            items={data}
+            icons={icons}
             checkboxProps={{ color: 'primary' }}
-            fieldComponent={state.fieldComponent}
-            icons={state.icons}
+            fieldComponent={fieldOptions => {
+              switch (fieldOptions.field) {
+                case 'Actions':
+                  return (
+                    <TableCell className="actioncontainer">
+                      <CloudDownload className={classes.actionicon} />
+                      <OpenInBrowser className={classes.actionicon} />
+                      <Edit />
+                    </TableCell>
+                  )
+                // no default
+              }
+              return null
+            }}
+            fieldsToDisplay={['DisplayName', 'CreatedBy', 'ModificationDate', 'Size', 'Actions']}
+            orderBy={'DisplayName'}
+            orderDirection={'asc'}
           />
         </Grid>
       </Grid>
